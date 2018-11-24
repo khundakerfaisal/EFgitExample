10246d95da8926280162d140dbbac6fdf180d6ef
Foysal <foysal2rox112@gmail.com>
2018-11-24 19:58:13 +0600

    new class added

--- gitsampleConsoleApp/Class1.cs
+++ gitsampleConsoleApp/Class1.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace gitsampleConsoleApp
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-           Student student= new Student();
-
-        }
     }
 }
